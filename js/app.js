01d253de67091b47ce16e937ee94e036015aa1f1
tamushun <shuntamu@gmail.com>
2014-09-24 06:47:01 +0900

    BackboneでView作成

--- js/app.js
+++ js/app.js
@@ -1,34 +1,22 @@
 (function() {
 
-//Model
+// Model
 
 var Task = Backbone.Model.extend({
 	defaults: {
 		title: "do something!",
 		completed: false
-	},
-	validate: function(attrs) {
-		if (_.isEmpty(attrs.title)) {
-			return "title must not be empty!";
-		}
-	},
-	toggle: function() {
-		this.set('completed', !this.get('completed'));
 	}
 });
 
-var task1 = new Task({
-//	title: 'do it!',
-	completed: true
+var task = new Task();
+
+// View
+
+var TaskView = Backbone.View.extend({
+	tagName: 'li'
 });
-
-//task1.set('title', 'newTitle');
-//var title = task1.get('title')
-//console.log(title);
-
-console.log(task1.toJSON());
-task1.set({title: ''});
-//task1.toggle();
-console.log(task1.toJSON());
+var taskView = new TaskView({ model: task });
+console.log(taskView.el);
 
 })();
